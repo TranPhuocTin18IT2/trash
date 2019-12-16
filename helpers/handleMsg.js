19636b7909324c842307b48798e802ff3a948699
TranPhuocTin18IT2 <tptin.18it2@sict.udn.vn>
2019-12-17 03:07:20 +0700

    heroku 0.1.7

--- helpers/handleMsg.js
+++ helpers/handleMsg.js
@@ -139,7 +139,7 @@ const tfjs_AI = async (fbUserMsg,senderID) =>{
       .argMax(1)
       .dataSync(0)
     console.log(predictions)
-    if(predictions.lenght>1) predictions = []
+    if(predictions.lenght>1) predictions.lenght = 0
     await handleMsg(train.types[predictions],senderName,senderID)
 }
 const getSenderInformation = (senderID,cb) =>{
