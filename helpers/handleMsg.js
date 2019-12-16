ae7bfe50555920c8d0ab41837b38c6e601f00c7d
TranPhuocTin18IT2 <tptin.18it2@sict.udn.vn>
2019-12-17 02:10:04 +0700

    heroku 0.1.1

--- helpers/handleMsg.js
+++ helpers/handleMsg.js
@@ -1,19 +1,19 @@
-const cfg = require('./config');
-const timeout = 3000;
-const request = require('request');
+const cfg = require('./config')
+const timeout = 3000
+const request = require('request')
 const tf = require('@tensorflow/tfjs-node')
 const train = require('../training')
 const predict = require('../predict')
 // connect to atlas mongodb
 
 module.exports.handleMessage = (sender_psid, receivedMsg)=>{
-    let response ;
+    let response 
     if(receivedMsg.text){
-        console.log(receivedMsg.text);
+        console.log(receivedMsg.text)
             tfjs_AI(receivedMsg.text,sender_psid)
             // response = {"text": `You sent the message: "${receivedMsg.text}". Now send me an image!`}
         }else if(receivedMsg.attachments){
-        let attachment_url = receivedMsg.attachments[0].payload.url;
+        let attachment_url = receivedMsg.attachments[0].payload.url
 /*message : */
      response = {
                 // Get the URL of the message attachment
@@ -41,29 +41,29 @@ module.exports.handleMessage = (sender_psid, receivedMsg)=>{
             }
           }
         }
-        // callSendAPI(sender_psid, response);
+        // callSendAPI(sender_psid, response)
     }
     // Send the response message
-    callSendAPI(sender_psid, response);
-};
+    callSendAPI(sender_psid, response)
+}
  // Handle postbacks
 module.exports.handlePostback = (sender_psid , received_postback)=>{
-    let response;
-    let payload = received_postback.payload;
+    let response
+    let payload = received_postback.payload
     switch (payload) {
         case 'GET_STARTED': {
-            response = { "text": "Bắt đầu"};
-            break;
+            response = { "text": "Bắt đầu"}
+            break
         }
         case 'yes': {
-            response = { "text": "Thanks" };
-            callSendAPI(sender_psid, response);
-            break;
+            response = { "text": "Thanks" }
+            callSendAPI(sender_psid, response)
+            break
         }
         case 'no': {
             response = { "text": "Oops, try send another image" }
-            callSendAPI(sender_psid, response);
-            break;
+            callSendAPI(sender_psid, response)
+            break
         }
         case 'GO_WILOKE': {
             response = {
@@ -82,18 +82,18 @@ module.exports.handlePostback = (sender_psid , received_postback)=>{
                     }
                     console.log(response)
                     callSendAPI(sender_psid, response)
-                }, timeout);
-            });
+                }, timeout)
+            })
 
-            break;
+            break
         }
         case 'NOT_GO_WILOKE': {
             response = { "text": "OK See you again" }
-            callSendAPI(sender_psid, response);
-            break;
+            callSendAPI(sender_psid, response)
+            break
         }
     }
-};
+}
 const callSendAPI = (sender_psid,response,cb=null)=>{
      // Construct the message body
     let request_body = {
@@ -101,7 +101,7 @@ const callSendAPI = (sender_psid,response,cb=null)=>{
             "id":sender_psid
         },
         "message":response
-    };
+    }
        // Send the HTTP request to the Messenger Platform
     request(
         {
@@ -114,9 +114,9 @@ const callSendAPI = (sender_psid,response,cb=null)=>{
         (err,res,body)=>{
             if(!err){
                 if(cb){
-                    cb();
+                    cb()
                 }
-                console.log("message sent!");
+                console.log("message sent!")
             }else{
                 console.error("Unable to send message"+err);
             }
@@ -127,19 +127,19 @@ const callSendAPI = (sender_psid,response,cb=null)=>{
 const tfjs_AI = async (fbUserMsg,senderID) =>{
     let senderName = ""
     let data = predict.matrixWeights(fbUserMsg) 
-    let loadmodel = await tf.loadLayersModel("file://model/model.json");
+    let loadmodel = await tf.loadLayersModel("file://model/model.json")
     await loadmodel.weights.forEach(element => {
-      console.log(element.name, element.shape);
-    });
+      console.log(element.name, element.shape)
+    })
     await getSenderInformation(senderID,(senderInfo)=>{
         senderName = senderInfo.first_name
     })
     let pridictTensor = loadmodel
       .predict(tf.tensor2d(data))
       .argMax(1)
-      .dataSync(0);
-    console.log(train.types[pridictTensor]);
-    await handleMsg(train.types[pridictTensor],senderName,senderID);
+      .dataSync(0)
+    console.log(pridictTensor)
+    await handleMsg(train.types[pridictTensor],senderName,senderID)
 }
 const getSenderInformation = (senderID,cb) =>{
     return request(
@@ -153,39 +153,39 @@ const getSenderInformation = (senderID,cb) =>{
       },
       (err, response, body) => {
         if (!err) {
-          return cb(JSON.parse(body));
+          return cb(JSON.parse(body))
         }
       }
-    );
+    )
 }
 
 const handleMsg = (tfjs_data,senderName,senderID)=>{
     if(tfjs_data=='greetings'){
          let response = {
            text: `Chào bạn ${senderName}, tôi có thể giúp gì cho bạn`
-         };
-         callSendAPI(senderID, response);
-         return;
+         }
+         callSendAPI(senderID, response)
+         return
     }
     if(tfjs_data=='weather'){
         let response = {
           text: 'thời tiết hôm nay là'
-        };
-        callSendAPI(senderID, response);
-        return;
+        }
+        callSendAPI(senderID, response)
+        return
     }
     if (tfjs_data == "regards") {
       let response = {
         text: 'không có gì '
-      };
-      callSendAPI(senderID, response);
-      return;
+      }
+      callSendAPI(senderID, response)
+      return
     }
     if (tfjs_data == "recommendations") {
       let response = {
         text: `Trời hôm nay đẹp lắm ${senderName}`
-      };
-      callSendAPI(senderID, response);
-      return;
+      }
+      callSendAPI(senderID, response)
+      return
     }
 }
