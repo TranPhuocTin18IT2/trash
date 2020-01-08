d21e94d8cfef251a9ff553c34a1fd7dd91a84a17
TranPhuocTin18IT2 <tptin.18it2@sict.udn.vn>
2020-01-08 16:03:34 +0700

    hien thi webhook_event

--- controller/index.js
+++ controller/index.js
@@ -38,11 +38,11 @@ module.exports.postMessage = (req,res)=>{
       webhook_event = entry.messaging[0];
       let senderId = webhook_event.sender.id ; 
       if(webhook_event.message){
-        console.log(senderId,JSON.stringify(webhook_event.message))
+        console.log(senderId,JSON.stringify(webhook_event))
         helper.handleMessage(senderId,webhook_event.message);
       }
       else if(webhook_event.postback) {
-        console.log(senderId,JSON.stringify(webhook_event.postback))
+        console.log(senderId,JSON.stringify(webhook_event))
         helper.handlePostback(senderId,webhook_event.postback);
       }
     });
