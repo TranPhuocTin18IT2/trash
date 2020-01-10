060e8043f3eaffa39f9ceb2787329d7aa77bbfa7
TranPhuocTin18IT2 <tptin.18it2@sict.udn.vn>
2020-01-10 22:32:24 +0700

    non db

--- generated_model.js
+++ generated_model.js
@@ -1,20 +1,20 @@
-const tf = require('@tensorflow/tfjs-node')
-const handle_data = require('./handle_data')
-const mailModel = require('./models/mailbox')
-const typeModel = require('./models/typesbox')
-let type_list = handle_data.typeList()
-let types_Input = handle_data.fill_types(type_list)
-let dictionary = handle_data.create_Dictionary()
-let wm = handle_data.weight_matrix()
-let vectors = handle_data.create_vectors(wm)
-console.log(vectors)
-// ======================================
-let word_Input_Tensor = tf.tensor2d(vectors)
-let type_Tensor = tf.tensor1d(types_Input, 'int32')
-let type_input_Tensor = tf.oneHot(type_Tensor,type_list.length)
+    const tf = require('@tensorflow/tfjs-node')
+    const handle_data = require('./handle_data')
+    let type_list = handle_data.typeList()
+    let types_Input = handle_data.fill_types(type_list)
+    let dictionary = handle_data.create_Dictionary()
+    let wm = handle_data.weight_matrix()
+    let vectors = handle_data.create_vectors(wm)
+    // console.log(vectors)
+    // ======================================
+    let word_Input_Tensor = tf.tensor2d(vectors)
+    // console.log(word_Input_Tensor)
+    let type_Tensor = tf.tensor1d(types_Input, 'int32')
+    // console.log(type_Tensor)
+    let type_input_Tensor = tf.oneHot(type_Tensor,type_list.length)
+    // console.log(type_input_Tensor)
     //tạo models
     let model = tf.sequential()
-
     //thêm hiddenlayer
     let hiden = tf.layers.dense({
         inputShape: dictionary.length,
@@ -43,8 +43,8 @@ let type_input_Tensor = tf.oneHot(type_Tensor,type_list.length)
     // models.weights.forEach(w=>{
     //     console.log(w.name, w.shape);
     // })
-async function train(){
-    await model.fit(word_Input_Tensor,type_input_Tensor,options)
-    await model.save('file://model')
-}
-train()
+    async function train(){
+        await model.fit(word_Input_Tensor,type_input_Tensor,options)
+        await model.save('file://model')
+    }
+    train()
--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const assert = require('assert')
 const uri =
     "mongodb://dbSICT:sictK18@anonymous-shard-00-01-app1j.mongodb.net:27017/weather-chat-bot?ssl=true&replicaSet=anonymous-shard-0&authSource=admin"
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    assert.equal(null, err)
-    console.log('connected')
-    require('./generated_model')
-})
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+//     assert.equal(null, err)
+//     console.log('connected')
+//     require('./generated_model')
+// })
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
